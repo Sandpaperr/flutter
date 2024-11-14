65bd4fc4d496b1c0a316b1dbb5dcaf5a373d3c0d
Leandro Russo <russo.leand@gmail.com>
2024-11-15 08:33:49 +1000

    Columns and Rows

--- my_card_flutter/lib/main.dart
+++ my_card_flutter/lib/main.dart
@@ -13,13 +13,34 @@ class MyApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.blue,
-          body: Container(
-            height: 100,
-            width: 100,
-            margin: const EdgeInsets.all(20.0),
-            padding: const EdgeInsets.only(top: 10.0),
-            color: Colors.white,
-            child: const Text("Hello"),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                //margin: const EdgeInsets.all(20.0),
+                //padding: const EdgeInsets.only(top: 10.0),
+                color: Colors.white,
+                child: const Text("Container 1"),
+              ),
+              // for spacing with specific height
+              const SizedBox(height: 10,),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+                child: const Text("Container 2"),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                child: const Text("Container 3"),
+              ),
+
+
+            ],
           ),
         ),
       ),
