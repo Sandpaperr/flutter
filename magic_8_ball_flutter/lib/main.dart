880428ceb62df7630f4130a7cf4a48c3f2c7eaaa
Leandro Russo <russo.leand@gmail.com>
2024-11-16 15:58:19 +1000

    started ball project

---
+++ magic_8_ball_flutter/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text(
+          'Ask Me Anything',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: () {  },
+    child: Center(child: Image.asset('images/ball1.png')),
+    );
+  }
+}
