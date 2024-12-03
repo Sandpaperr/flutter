e502c3eb2df8a829d88bae758cb3ecbd508c7e38
Leandro Russo <russo.leand@gmail.com>
2024-12-03 16:04:10 +0000

    exported the roundButton to the components directory

--- flash_chat_flutter/lib/screens/welcome_screen.dart
+++ flash_chat_flutter/lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flash_chat_flutter/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat_flutter/screens/registration_screen.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat_flutter/components/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id ='welcome_screen';
@@ -94,32 +95,3 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
   }
 }
 
-class RoundedButton extends StatelessWidget {
-  const RoundedButton({this.color, this.buttonTitle, this.onPressed});
-
-  final Color? color;
-  final String? buttonTitle;
-  final VoidCallback? onPressed;
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        elevation: 5.0,
-        color: color,
-        borderRadius: BorderRadius.circular(30.0),
-        child: MaterialButton(
-          onPressed: onPressed,
-          minWidth: 200.0,
-          height: 42.0,
-          child: Text(
-            buttonTitle!,
-          ),
-        ),
-      ),
-    );
-  }
-}
