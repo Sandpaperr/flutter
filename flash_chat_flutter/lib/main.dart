7e49db6da13b25ce291cd1fa49b8295e1a4a4738
Leandro Russo <russo.leand@gmail.com>
2024-12-05 23:25:16 +0000

    solvd problem with firebase init

--- flash_chat_flutter/lib/main.dart
+++ flash_chat_flutter/lib/main.dart
@@ -1,15 +1,25 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flash_chat_flutter/firebase_options.dart';
 
-
-void main() => runApp(FlashChat());
+void main() async{
+  // this is needed to run firebase initialize otherwise it won't work
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)  {
+
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         textTheme: const TextTheme(
--- flash_chat_flutter/lib/screens/registration_screen.dart
+++ flash_chat_flutter/lib/screens/registration_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat_flutter/components/rounded_button.dart';
 import 'package:flash_chat_flutter/constants.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'chat_screen.dart';
 class RegistrationScreen extends StatefulWidget {
   static const String id ='registration_screen';
-
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  late String email;
+  late String password;
+  final _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,28 +35,42 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
               onChanged: (value) {
-                //Do something with the user input.
+                email = value;
               },
               decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
             ),
-            SizedBox(
+            const SizedBox(
               height: 8.0,
             ),
             TextField(
+              textAlign: TextAlign.center,
+              obscureText: true,
               onChanged: (value) {
-                //Do something with the user input.
+                password = value;
               },
               decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your password'),
             ),
-            SizedBox(
+            const SizedBox(
               height: 24.0,
             ),
             RoundedButton(
               color: Colors.blueAccent,
               buttonTitle: 'Register',
-              onPressed: () {
-                //Implement registration functionality.
+              onPressed: () async{
+                // print(email);
+                // print(password);
+                try{
+                  final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+                  if (newUser != null){
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                }
+                catch (e) {
+                  print(e);
+                }
               },
             )
           ],
