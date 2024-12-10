6422d0379ac7a19971d4983489b7c303b013dc92
Leandro Russo <russo.leand@gmail.com>
2024-12-10 13:40:56 +0000

    initial ui for todo page

--- todo_things/lib/screens/tasks_screen.dart
+++ todo_things/lib/screens/tasks_screen.dart
@@ -3,8 +3,81 @@ import 'package:flutter/material.dart';
 class TaskScreen extends StatelessWidget {
   const TaskScreen({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        shape: const CircleBorder(),
+        backgroundColor: Colors.lightBlueAccent,
+        child: const Icon(Icons.add),
+        onPressed: (){},
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child:Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+
+                ),
+
+                SizedBox(height: 10.0,),
+
+                // Title
+                Text(
+                  'ToDoThings',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+
+                  ),
+                ),
+
+                //text with to-do left
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+
+
+
+              ],
+            ),
+          ),
+          // Folder-like container to put to-do inside
+          Expanded(
+            child: Container(
+              height: 300.0,
+              decoration:const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30.0),
+                  topLeft: Radius.circular(30.0),
+
+
+                ),
+
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
